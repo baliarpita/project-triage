7b5c31458fff0776c699a1ce3ee4c03141e89afc
Arpita Bali <baliarpita@gmail.com>
2019-09-09 11:27:12 -0400

    Vue added. Patient waiting queue updated

---
+++ app/public/dummy.php
@@ -0,0 +1,23 @@
+[
+    {
+        "patientGuid": "REALLY_LONG_1234",
+        "firstName": "Arpita",
+        "lastName": "Bali",
+        "dob": "1995-09-01",
+        "sexAtBirth": "F"
+    },
+    {
+        "patientGuid": "REALLY_LONG_1235",
+        "firstName": "Austin",
+        "lastName": "Rodgers",
+        "dob": "1996-09-01",
+        "sexAtBirth": "M"
+    },
+    {
+        "patientGuid": "REALLY_LONG_1236",
+        "firstName": "Terry",
+        "lastName": "Bae",
+        "dob": "1996-09-01",
+        "sexAtBirth": "M"
+    }
+]
---
+++ app/public/js/app.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+    el: '#patientWaitingApp',
+    data: {
+        message: 'Hello Vue!',
+        patients: [
+            {
+                patientGuid: "REALLY_LONG_1234",
+                firstName: "Arpita",
+                lastName: "Bali",
+                dob: "1995-09-01",
+                sexAtBirth: "F"
+            },
+            {
+                patientGuid: "REALLY_LONG_1235",
+                firstName: "Austin",
+                lastName: "Rodgers",
+                dob: "1996-09-01",
+                sexAtBirth: "M"
+            },
+            {
+                patientGuid: "REALLY_LONG_1236",
+                firstName: "Terry",
+                lastName: "Bae",
+                dob: "1996-09-01",
+                sexAtBirth: "M"
+            }
+        ]
+
+    }
+});
