a627a0ae75e13f5eafec48547f696596b98d8266
Arpita Bali <baliarpita@gmail.com>
2019-09-16 11:02:32 -0400

    Updated

--- app/public/dummy.php
+++ app/public/dummy.php
@@ -1,26 +1,26 @@
 [
-    {
-        "patientGuid": "REALLY_LONG_1234",
-        "firstName": "Arpita",
-        "lastName": "Bali",
-        "dob": "1995-09-01",
-        "sexAtBirth": "F",
-        priority: "medium"
-    },
-    {
-        "patientGuid": "REALLY_LONG_1235",
-        "firstName": "Austin",
-        "lastName": "Rodgers",
-        "dob": "1996-09-01",
-        "sexAtBirth": "M",
-        priority: "high"
-    },
-    {
-        "patientGuid": "REALLY_LONG_1236",
-        "firstName": "Terry",
-        "lastName": "Bae",
-        "dob": "1996-09-01",
-        "sexAtBirth": "M",
-        priority: "low"
-    }
+  {
+    "patientGuid": "SOME-REALLY-LONG-1234",
+    "firstName": "Sylvia",
+    "lastName": "Hernandez",
+    "dob": "2012-09-01",
+    "sexAtBirth": "F",
+    "priority": "high"
+  },
+  {
+    "patientGuid": "SOME-REALLY-SHORT-5678",
+    "firstName": "Vish",
+    "lastName": "Balasubramanian",
+    "dob": "1950-12-15",
+    "sexAtBirth": "M",
+    "priority": "medium"
+  },
+  {
+    "patientGuid": "SOME-UNIQUE-ABCDE1",
+    "firstName": "John",
+    "lastName": "Doe",
+    "dob": "1997",
+    "sexAtBirth": "M",
+    "priority": "low"
+  }
 ]
