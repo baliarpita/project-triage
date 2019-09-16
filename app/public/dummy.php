a24271c2024ab304a4ab4f88b1390b4dc90e457d
Arpita Bali <baliarpita@gmail.com>
2019-09-16 10:00:06 -0400

    Added random data

--- app/public/dummy.php
+++ app/public/dummy.php
@@ -4,20 +4,23 @@
         "firstName": "Arpita",
         "lastName": "Bali",
         "dob": "1995-09-01",
-        "sexAtBirth": "F"
+        "sexAtBirth": "F",
+        priority: "medium"
     },
     {
         "patientGuid": "REALLY_LONG_1235",
         "firstName": "Austin",
         "lastName": "Rodgers",
         "dob": "1996-09-01",
-        "sexAtBirth": "M"
+        "sexAtBirth": "M",
+        priority: "high"
     },
     {
         "patientGuid": "REALLY_LONG_1236",
         "firstName": "Terry",
         "lastName": "Bae",
         "dob": "1996-09-01",
-        "sexAtBirth": "M"
+        "sexAtBirth": "M",
+        priority: "low"
     }
 ]
