ea60a106d0498ba88871ef467f705e07596a51ee
Arpita Bali <baliarpita@gmail.com>
2019-09-18 10:20:43 -0400

    Patient Record added

---
+++ app/public/js/patient.record.js
@@ -0,0 +1,19 @@
+var patientRecordApp = new Vue({
+    el: '#patientRecordApp',
+    data: {
+      patients: []
+    },
+    methods: {
+      fetchPatients() {
+        fetch('dummy.php')
+        .then(response => response.json())
+        .then(json => {patientRecordApp.patients = json});
+  
+      }
+    },
+    created: function() {
+      this.fetchPatients();
+    }
+  
+  })
+  
--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -9,11 +9,6 @@ var waitingApp = new Vue({
       .then(response => response.json())
       .then(json => {waitingApp.patients = json});
 
-      // Means the same at this
-      // fetch('https://randomuser.me/api/')
-      // .then(function(response) {return response.json()})
-      // .then(function(json) {waitingApp.people = json});
-
     }
   },
   created: function() {
