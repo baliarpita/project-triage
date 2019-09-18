85686a367563849f09d48814eae05f5696c73995
Arpita Bali <baliarpita@gmail.com>
2019-09-18 13:50:18 -0400

    Form added

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -1,19 +1,35 @@
 var patientRecordApp = new Vue({
-    el: '#patientRecordApp',
-    data: {
-      patients: []
-    },
-    methods: {
-      fetchPatients() {
-        fetch('dummy.php')
-        .then(response => response.json())
-        .then(json => {patientRecordApp.patients = json});
-  
-      }
-    },
-    created: function() {
-      this.fetchPatients();
+  el: '#patientRecordApp',
+  data: {
+    patients: [],
+    formPatient: {
+      firstName: "",
+      lastName: "",
+      dob: "",
+      sexAtBirth: ""
     }
-  
-  })
-  
+  },
+  methods: {
+    fetchPatients() {
+      fetch('dummy.php')
+        .then(response => response.json())
+        .then(json => { patientRecordApp.patients = json });
+    },
+    handleCreatePatientRecord() {
+      // fetch(url, {method:"post", data:thisFormPatient}
+      // This is what should happen when a data store is added to 
+      // project-triage. Then, we want to display it:
+      this.patients.push(this.formPatient);
+      this.formPatient = {
+                            firstName: "",
+                            lastName: "",
+                            dob: "",
+                            sexAtBirth: ""
+                        };
+    }
+  },
+  created: function () {
+    this.fetchPatients();
+  }
+
+})
