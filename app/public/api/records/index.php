18f670f4968af16233bf03f72f11cecffc1312bf
Arpita Bali <baliarpita@gmail.com>
2019-10-02 12:05:22 -0400

    Updated code. Adeded PHP

--- app/public/api/records/index.php
+++ app/public/api/records/index.php
@@ -3,16 +3,13 @@
 // Step 1: Get a datase connection from our help class
 $db = DbConnection::getConnection();
 
+// -> for instance
+// :: is for static/factory methods in class
+
 // Step 2: Create & run the query
 $stmt = $db->prepare('SELECT * FROM Patient');
 $stmt->execute();
 $patients = $stmt->fetchAll();
-
-// patientGuid VARCHAR(64) PRIMARY KEY,
-// firstName VARCHAR(64),
-// lastName VARCHAR(64),
-// dob DATE DEFAULT NULL,
-// sexAtBirth CHAR(1) DEFAULT ''
 
 // Step 3: Convert to JSON
 $json = json_encode($patients, JSON_PRETTY_PRINT);
